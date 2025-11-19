e47ca2034eecf9544813d1182329194ad1d7ad56
AmandaZ2106 <romeogcruz@outlook.com>
2025-11-19 17:45:18 +0000

    Private class

---
+++ Private.py
@@ -0,0 +1,9 @@
+class Myclass:
+    __People=32
+    def __where(self):
+        print("I am in my class")
+    def __teacher(self):
+        print("My teacher is funny")
+cls=Myclass()
+cls.__where()
+cls.__teacher
