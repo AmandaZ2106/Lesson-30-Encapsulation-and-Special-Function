d453536f76f9a4e31481d9e746a892dffd5d42f1
AmandaZ2106 <romeogcruz@outlook.com>
2025-11-19 17:55:11 +0000

    Computer

---
+++ Computer.py
@@ -0,0 +1,13 @@
+class Computer:
+    def __init__(self):
+       self.__maxprice=990
+    def sell(self):
+        print("Selling for:",self.__maxprice)
+    def price(self,price):
+        self.__maxprice=price
+        print("Price:",price)
+com=Computer()
+com.__maxprice=100
+com.sell
+com.price(2000)
+    
